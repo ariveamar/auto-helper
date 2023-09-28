93de5b904091b48ff322fc2f4bb4a36b31aba0a2
ariveamar <41986809+ariveamar@users.noreply.github.com>
2023-09-28 08:39:56 +0700

    Create re-coonfig.sh

---
+++ re-coonfig.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ansible-playbook tasks/configure_dhcpd.yml
+ansible-playbook tasks/configure_bind_dns.yml
+rm -rf /var/lib/tftpboot/pxelinux.cfg/*
+ansible-playbook tasks/configure_tftp_pxe.yml
