717a2845db101e5f79a4f1e60c9d25254a7d946c
ariveamar <41986809+ariveamar@users.noreply.github.com>
2023-09-28 08:22:56 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ setenforce 0
 echo "Please confirm set main variable first done ?"
 read inputs
 echo $inputs
-vim handlers/main.yml
 ansible-playbook tasks/configure_dhcpd.yml
 cp set-dns-serial.sh /usr/local/bin/
 chmod +x /usr/local/bin/set-dns-serial.sh
@@ -23,7 +22,6 @@ mkdir -p  /var/lib/tftpboot/pxelinux.cfg
 cp -rvf /usr/share/syslinux/* /var/lib/tftpboot
 mkdir -p /var/lib/tftpboot/rhcos
 mkdir -p /var/www/html/rhcos/
-chmod 775 /var/www/html/rhcos 
 wget -O /var/lib/tftpboot/rhcos/initramfs.img https://rhcos.mirror.openshift.com/art/storage/prod/streams/4.12/builds/412.86.202308081039-0/x86_64/rhcos-412.86.202308081039-0-live-initramfs.x86_64.img
 wget -O  /var/lib/tftpboot/rhcos/kernel https://rhcos.mirror.openshift.com/art/storage/prod/streams/4.12/builds/412.86.202308081039-0/x86_64/rhcos-412.86.202308081039-0-live-kernel-x86_64
 wget -O /var/www/html/rhcos/rootfs.img https://rhcos.mirror.openshift.com/art/storage/prod/streams/4.12/builds/412.86.202308081039-0/x86_64/rhcos-412.86.202308081039-0-live-rootfs.x86_64.img
@@ -31,8 +29,19 @@ wget -O /root/openshift-install-linux.tar.gz https://mirror.openshift.com/pub/op
 wget -O /root/openshift-client-linux.tar.gz https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/4.12.35/openshift-client-linux.tar.gz
 tar -xvf /root/openshift-install-linux.tar.gz
 tar -xvf /root/openshift-client-linux.tar.gz
+chmod -R 775 /var/www/html/rhcos 
+chmod -R 775 /var/lib/tftpboot/rhcos
 sed -i 's\80\8080\g' /etc/httpd/conf/httpd.conf
 sudo systemctl enable httpd --now
 ansible-playbook tasks/configure_tftp_pxe.yml
-
 sudo mkdir -p /var/www/html/ignition
+echo "Please confirm set install-config.yaml has been done ?"
+read inputs
+echo $inputs
+mkdir /ocp-prod
+cp install-config.yaml /ocp-prod/
+sh /root/openshift-install create manifests --dir  /ocp-prod
+sed -i 's\true\false\g' /ocp-prod/manifests/cluster-scheduler-02-config.yml
+sh /root/openshift-install create ignition-configs --dir /ocp-prod
+cp /ocp-prod/*.ign /var/www/html/ignition
+chmod 777 /var/www/html/ignition/*ign
